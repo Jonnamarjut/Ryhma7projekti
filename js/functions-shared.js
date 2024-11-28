4e71533ca3073d30095793a6bfd77dee7977818a
Olivia Kwok <n3kwoi00@students.oamk.fi>
2024-11-28 12:19:42 +0200

    Muistipelin lisäys ja functions-shared.js muokkaus

--- js/functions-shared.js
+++ js/functions-shared.js
@@ -11,4 +11,49 @@ function displayLatestScoreClothes() {
 }
 displayLatestScoreClothes();
 
+//Clothes points
 
+const scoresOfClothes = localStorage.getItem('')
+document.getElementById('vaatteetpisteet').innerHTML = scoresOfClothes
+
+
+//Animals points
+
+const scoresOfAnimals = localStorage.getItem('animals')
+document.getElementById('elaimetpisteet').innerHTML = scoresOfAnimals
+
+//Colours points
+
+const scoresOfColours = localStorage.getItem('')
+document.getElementById('varitpisteet').innerHTML = scoresOfColours
+
+
+//Adjectives points
+
+const scoresOfAdjectives = localStorage.getItem('')
+document.getElementById('adjektiivitpisteet').innerHTML = scoresOfAdjectives
+
+
+//Relaxing points
+
+const scoresOfRelaxing = localStorage.getItem('')
+document.getElementById('rentoutuminenpisteet').innerHTML = scoresOfRelaxing
+
+
+// Exercising points
+
+const scoresOfExercising = localStorage.getItem('')
+document.getElementById('liikuntapisteet').innerHTML = scoresOfExercising
+
+
+//Total score
+
+let score = parseInt(localStorage.getItem('animals'))
+
+//  score += parseInt(localStorage.getItem(''))
+//  score += parseInt(localStorage.getItem(''))
+//  score += parseInt(localStorage.getItem(''))
+//  score += parseInt(localStorage.getItem(''))
+//  score += parseInt(localStorage.getItem(''))
+
+document.getElementById('allscores').innerHTML = score
